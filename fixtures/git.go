c42e6e59d65293c9a1840c058f762c617cbe42c1
koooooo-7 <369491420@qq.com>
2020-08-20 21:54:18 +0800

    [update] prank, cant get the success xD.

--- fixtures/git.go
+++ fixtures/git.go
@@ -1,38 +1,37 @@
 package fixtures
 
-var pullConsole = `
-"cd ~/git"
-"git clone https://github.com/chenjiandongx/oscar.git"
-"Cloning into 'oscar'..."
-"remote: Enumerating objects: 101, done."
-"remote: Counting objects: 100% (101/101), done."
-"remote: Compressing objects: 100% (71/71), done."
-"remote: Total 101 (delta 53), reused 77 (delta 29), pack-reused 0"
-"Receiving objects: 100% (101/101), 244.37 KiB | 23.00 KiB/s, done."
-"Resolving deltas: 100% (53/53), done."
-`
-
-var statusConsole = `
-git status
-On branch oscar-git
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git checkout -- <file>..." to discard changes in working directory)
-
-        modified:   git.go
-
-no changes added to commit (use "git add" and/or "git commit -a")
-
-`
-
-var addConsole = `
-git add git.go
-warning: LF will be replaced by CRLF in fixtures/git.go.
-The file will have its original line endings in your working directory.
-`
-
-
-
-var GitPullConsole = readLines(pullConsole)
-var GitStatusConsole = readLines(statusConsole)
-var GitAddConsole = readLines(addConsole)
+var GitConsole = [...]string{
+	"cd ~/git",
+	"git clone https://github.com/chenjiandongx/oscar.git",
+	"Cloning into 'oscar'...",
+	"remote: Enumerating objects: 101, done.",
+	"remote: Counting objects: 100% (101/101), done.",
+	"remote: Compressing objects: 100% (71/71), done.",
+	"remote: Total 101 (delta 53), reused 77 (delta 29), pack-reused 0",
+	"Receiving objects: 100% (101/101), 244.37 KiB | 23.00 KiB/s, done.",
+	"Resolving deltas: 100% (53/53), done.",
+	"",
+	"git status",
+	"On branch oscar-git",
+	"Changes not staged for commit:",
+	"",
+	"  (use \"git add <file>...\" to update what will be committed)",
+	"  (use \"git checkout -- <file>...\" to discard changes in working directory)",
+	"",
+	"        modified:   git.go",
+	"",
+	"no changes added to commit (use \"git add\" and/or \"git commit -a\")",
+	"",
+	"git add . && git commit -m \"update git.go\"",
+	"warning: LF will be replaced by CRLF in fixtures/git.go.",
+	"The file will have its original line endings in your working directory.",
+	"[oscar-git da3d630] update git.go",
+	"1 file changed, 24 insertions(+), 2 deletions(-)",
+	"",
+	"git push",
+	"ssh: connect to host github.com port 22: Connection timed out",
+	"fatal: Could not read from remote repository.",
+	"",
+	"Please make sure you have the correct access rights",
+	"and the repository exists.",
+}
