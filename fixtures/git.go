cd5515b0e401625111ee19eda087e81c46812287
koooooo-7 <369491420@qq.com>
2020-08-20 21:09:36 +0800

    [refine] move constants to fixture.

---
+++ fixtures/git.go
@@ -0,0 +1,15 @@
+package fixtures
+
+var console=`
+"cd ~/git"
+"git clone https://github.com/chenjiandongx/oscar.git"
+"Cloning into 'oscar'..."
+"remote: Enumerating objects: 101, done."
+"remote: Counting objects: 100% (101/101), done."
+"remote: Compressing objects: 100% (71/71), done."
+"remote: Total 101 (delta 53), reused 77 (delta 29), pack-reused 0"
+"Receiving objects: 100% (101/101), 244.37 KiB | 23.00 KiB/s, done."
+"Resolving deltas: 100% (53/53), done."
+`
+
+var GitConsole = readLines(console)
--- modules/git.go
+++ modules/git.go
@@ -2,22 +2,10 @@ package modules
 
 import (
 	"fmt"
-	"reflect"
+	"github.com/chenjiandongx/oscar/fixtures"
 )
 
 type git struct{}
-
-type gitConsoleLine struct {
-	c0 string
-	c1 string
-	c2 string
-	c3 string
-	c4 string
-	c5 string
-	c6 string
-	c7 string
-	c8 string
-}
 
 func (mod *git) Name() string {
 	return ModGit
@@ -25,22 +13,9 @@ func (mod *git) Name() string {
 
 func (mod *git) Display() {
 
-	gcl := gitConsoleLine{
-		"cd ~/git",
-		"git clone https://github.com/chenjiandongx/oscar.git",
-		"Cloning into 'oscar'...",
-		"remote: Enumerating objects: 101, done.",
-		"remote: Counting objects: 100% (101/101), done.",
-		"remote: Compressing objects: 100% (71/71), done.",
-		"remote: Total 101 (delta 53), reused 77 (delta 29), pack-reused 0",
-		"Receiving objects: 100% (101/101), 244.37 KiB | 23.00 KiB/s, done.",
-		"Resolving deltas: 100% (53/53), done.",
-	}
-
-	v := reflect.ValueOf(gcl)
-	for i := 0; i < 9; i++ {
+	for i := 0; i < len(fixtures.GitConsole); i++ {
 		SleepInMills(500, 1000)
-		fmt.Println(v.Field(i))
+		fmt.Println(fixtures.GitConsole[i])
 	}
 }
 
