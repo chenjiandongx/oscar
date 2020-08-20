4533de8396edc420158cbbe7c0f94f193239e6df
koooooo-7 <369491420@qq.com>
2020-08-20 21:18:55 +0800

    update git.go

--- fixtures/git.go
+++ fixtures/git.go
@@ -1,6 +1,6 @@
 package fixtures
 
-var console = `
+var pullConsole = `
 "cd ~/git"
 "git clone https://github.com/chenjiandongx/oscar.git"
 "Cloning into 'oscar'..."
@@ -12,4 +12,27 @@ var console = `
 "Resolving deltas: 100% (53/53), done."
 `
 
-var GitConsole = readLines(console)
+var statusConsole = `
+git status
+On branch oscar-git
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git checkout -- <file>..." to discard changes in working directory)
+
+        modified:   git.go
+
+no changes added to commit (use "git add" and/or "git commit -a")
+
+`
+
+var addConsole = `
+git add git.go
+warning: LF will be replaced by CRLF in fixtures/git.go.
+The file will have its original line endings in your working directory.
+`
+
+
+
+var GitPullConsole = readLines(pullConsole)
+var GitStatusConsole = readLines(statusConsole)
+var GitAddConsole = readLines(addConsole)
